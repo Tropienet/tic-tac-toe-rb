d69a4eddddcce84ab67a826f4e87e59c565def39
Tin Goricki <exibodude@gmail.com>
2023-02-28 18:48:44 +0100

    made board class

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -3,4 +3,27 @@ class Player
     @name = name
     @symbol = symbol
   end
+
+  def show_player
+    puts "Player name is #{@name} and symbol is #{@symbol}"
+  end
 end
+
+class Board
+  @@board = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ]
+
+  def show_board
+    puts "Board is \n
+          #{@@board[0]} | #{@@board[1]} | #{@@board[2]} \n
+          #{@@board[3]} | #{@@board[4]} | #{@@board[5]} \n
+          #{@@board[6]} | #{@@board[7]} | #{@@board[8]} \n"
+  end
+
+end
+
+tin = Player.new("tin", "x")
+gameboard = Board.new
+gameboard.show_board
+
+
+tin.show_player
