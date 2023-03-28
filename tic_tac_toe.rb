cf08e02e6de58e34f13fe5911816611c85bef982
Tin Goricki <exibodude@gmail.com>
2023-03-28 23:42:20 +0200

    Changed file name and method names to snake case

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,45 +1,43 @@
+# frozen_string_literal: true
+
 class Player
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
 
-  public
-  def logPlayerName
+  def log_player_name
     puts @name
   end
 end
 
 class Game
-  @@gameboard = [[0,1,2],
-                 [3,4,5],
-                 [6,7,8]]
+  @gameboard = [[0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]]
 
-  public
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
   end
 
-  def logPlayers
+  def log_players
     puts @player1.logPlayerName
     puts @player2.logPlayerName
   end
 
-  def displayBoard
-    @@gameboard.each_with_index do |row, index|
+  def display_board
+    @@gameboard.each_with_index do |row, _index|
       row.each_with_index do |tile, index|
         print tile
-        if index == 2
-          print "\n"
-        end
+        print "\n" if index == 2
       end
     end
   end
 end
 
-player1 = Player.new("tin", "x")
-player2 = Player.new("Toni", "o")
+player1 = Player.new('tin', 'x')
+player2 = Player.new('Toni', 'o')
 
 game = Game.new(player1, player2)
 
