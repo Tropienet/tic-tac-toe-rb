9ecf013c213f57bde4c89b0dfec939ed6ef6e344
Tin Goricki <exibodude@gmail.com>
2023-03-28 23:37:52 +0200

    Added a method to display the board

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -11,6 +11,11 @@ class Player
 end
 
 class Game
+  @@gameboard = [[0,1,2],
+                 [3,4,5],
+                 [6,7,8]]
+
+  public
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
@@ -21,6 +26,16 @@ class Game
     puts @player2.logPlayerName
   end
 
+  def displayBoard
+    @@gameboard.each_with_index do |row, index|
+      row.each_with_index do |tile, index|
+        print tile
+        if index == 2
+          print "\n"
+        end
+      end
+    end
+  end
 end
 
 player1 = Player.new("tin", "x")
@@ -28,4 +43,4 @@ player2 = Player.new("Toni", "o")
 
 game = Game.new(player1, player2)
 
-game.logPlayers
+game.displayBoard
